261fffe9d46f06b219be0b283351491f02809fba
Michael Koert <michaelkoert@gmail.com>
2026-02-04 17:00:47 -0500

    replaced webpack with vite. used jsx ext.

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const WS_URL = process.env.REACT_APP_WS_URL || 'ws://localhost/ws';
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost/api';
+export const WS_URL = import.meta.env.VITE_WS_URL || 'ws://localhost/ws';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost/api';
 
 export const CATEGORY_COLORS = {
   nature: '#4caf50',
